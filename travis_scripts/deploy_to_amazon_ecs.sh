e251d928b52d4b09c77536f2051fccb03366b4b3
sprinteiro <sprinteiro@gmail.com>
2018-02-11 11:52:54 +0000

    Correct typo in Amazon access and secret key variables value fetching

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,6 +1,6 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 ecs-cli configure --region eu-west-2 --cluster spmia-tmx-dev --default-launch-type EC2 --config-name spmia-tmx-dev
-ecs-cli configure profile --access-key AWS_ACCESS_KEY_ID --secret-key AWS_SECRET_ACCESS_KEY --profile-name  spmia-tmx-dev
+ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY --profile-name  spmia-tmx-dev
 
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
